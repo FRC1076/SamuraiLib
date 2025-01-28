547490524e78ab160b28c90f7db7ccc7c1a6acb0
Jesse Kane <jessekane66@gmail.com>
2025-01-28 14:40:11 -0500

    added FRC 1076 library to codebase

---
+++ src/main/java/lib/control/PathfollowingLQRHolonomicDriveController.java
@@ -0,0 +1,40 @@
+package lib.control;
+
+import com.pathplanner.lib.controllers.PathFollowingController;
+import com.pathplanner.lib.trajectory.PathPlannerTrajectoryState;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
+/** Provides an LQR holonomic controller in the interface shape of a PathFollowingController, for compatibility with pathplanner */
+public class PathfollowingLQRHolonomicDriveController extends LQRHolonomicDriveController implements PathFollowingController {
+
+    public PathfollowingLQRHolonomicDriveController(LQRHolonomicDriveControllerTolerances tolerances, double dt) {
+        super(tolerances,dt);
+    }
+
+    @Override
+    public void reset(Pose2d currentPose, ChassisSpeeds currentSpeeds) {
+        super.resetController();
+    }
+
+    @Override
+    public boolean isHolonomic() {
+        return true;
+    }
+
+    @Override
+    public ChassisSpeeds calculateRobotRelativeSpeeds(Pose2d currentPose, PathPlannerTrajectoryState targetState) {
+        double xFF = targetState.fieldSpeeds.vxMetersPerSecond;
+        double yFF = targetState.fieldSpeeds.vyMetersPerSecond;
+        var LQRFeedbacks = super.calculateRaw(currentPose,targetState.pose);
+        return ChassisSpeeds.fromFieldRelativeSpeeds(
+            LQRFeedbacks.get(0,0) + xFF, 
+            LQRFeedbacks.get(1,0) + yFF,
+            LQRFeedbacks.get(2,0),
+            currentPose.getRotation()
+        );
+
+    }
+
+}
