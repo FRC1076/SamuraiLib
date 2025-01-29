0ad5ccbd21a2723c4ff8a2ac498b0dc8542fd987
Jesse Kane <jessekane66@gmail.com>
2025-01-29 01:10:21 -0500

    Added in-subsystem command factories, with the proper end conditions

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -3,8 +3,13 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
+
 import static edu.wpi.first.units.Units.*;
+
+import java.util.function.DoubleSupplier;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
@@ -28,6 +33,19 @@ public class ElevatorSubsystem extends SubsystemBase {
         return inputs.elevatorHeightMeters;
     }
 
+    public Command applyPosition(double positionMeters) {
+        return new FunctionalCommand(
+            () -> setPosition(positionMeters),
+            () -> {},
+            (interrupted) -> {},
+            () -> Math.abs(positionMeters - getPositionMeters()) > ElevatorConstants.elevatorPositionToleranceMeters,
+            this
+        );
+    }
+    
+    public Command applyManualControl(DoubleSupplier controlSupplier) {
+        return run(() -> setVoltage(controlSupplier.getAsDouble() * ElevatorConstants.maxOperatorControlVolts));
+    }
 
     @Override
     public void periodic(){
--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.wrist;
 
+import java.util.function.DoubleSupplier;
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.WristConstants;
 
 public class WristSubsystem extends SubsystemBase {
     private final WristIO io;
@@ -33,6 +37,20 @@ public class WristSubsystem extends SubsystemBase {
 
     public double getAngleRadians() {
         return getAngle().getRadians();
+    }
+
+    public Command applyAngle(Rotation2d angle) {
+        return new FunctionalCommand(
+            () -> setPosition(angle),
+            () -> {}, 
+            (interrupted) -> {}, 
+            () -> Math.abs(angle.minus(getAngle()).getRadians()) > WristConstants.wristAngleToleranceRadians,
+            this
+        );
+    }
+
+    public Command applyManualControl(DoubleSupplier controlSupplier) {
+        return run(() -> setVoltage(controlSupplier.getAsDouble() * WristConstants.maxOperatorControlVolts));
     }
 
     @Override
