513560ba9c064132718d6007a3ca413617f49c00
Jesse Kane <jessekane66@gmail.com>
2025-02-01 18:55:57 -0500

    added safe deploy height to wristevator applyStateDynamic command

---
+++ src/main/java/frc/robot/subsystems/wristevator/Wristevator.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems.wristevator;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.SuperstructureConstants.WristevatorState;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.wrist.WristSubsystem;
+
+public class Wristevator {
+    public final ElevatorSubsystem m_elevator;
+    public final WristSubsystem m_wrist;
+
+    public Wristevator(ElevatorSubsystem elevator, WristSubsystem wrist) {
+        m_elevator = elevator;
+        m_wrist = wrist;
+    }
+
+    public void setKgConstants(double elevator_kG, double wrist_kG) {
+        m_elevator.setKg(elevator_kG);
+        m_wrist.setKg(wrist_kG);
+    }
+
+    /**
+     * Sequentially sets wrist to a safe angle, moves elevator to desired state, then sets wrist to desired state
+     */
+    public Command applyStateSafe(WristevatorState state, Rotation2d safeAngle) {
+        return Commands.sequence(
+            m_wrist.applyAngle(safeAngle),
+            m_elevator.applyPosition(state.elevatorHeightMeters),
+            m_wrist.applyAngle(state.wristAngle)
+        );
+    }
+
+    /** Allows the user to define a deploy safe height, so that the wrist can begin deploying as quickly as possible */
+    public Command applyStateDynamic(WristevatorState state, Rotation2d safeAngle, double wristDeploySafeHeightMeters) {
+        return Commands.parallel(
+            m_elevator.applyPosition(state.elevatorHeightMeters),
+            Commands.waitUntil(() -> m_elevator.getPositionMeters() >= wristDeploySafeHeightMeters)
+                .andThen(m_wrist.applyAngle(state.wristAngle))
+        ).beforeStarting(m_wrist.applyAngle(safeAngle));
+    }
+}
