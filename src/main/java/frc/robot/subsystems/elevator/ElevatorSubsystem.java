942eff6a0c1f7ee597d7484cfa7280afde30ed80
1076software <1076software@gmail.com>
2025-01-31 19:48:53 -0500

    added sysId to elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.elevator;
 import org.littletonrobotics.junction.Logger;
 
+import com.ctre.phoenix6.SignalLogger;
+
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
 import frc.robot.Constants.ElevatorConstants;
 
 import static edu.wpi.first.units.Units.*;
@@ -12,9 +16,26 @@ import static edu.wpi.first.units.Units.*;
 import java.util.function.DoubleSupplier;
 
 public class ElevatorSubsystem extends SubsystemBase {
+    /*To Do:
+        Add a separate set voltage function that utilizes kG
+    */
 
     private final ElevatorIO io;
     private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+    private final SysIdRoutine m_elevatorSysIdRoutine = new SysIdRoutine(
+        new SysIdRoutine.Config(
+            null,        // Use default ramp rate (1 V/s)
+            Volts.of(4), // Reduce dynamic step voltage to 4 V to prevent brownout
+            null,        // Use default timeout (10 s)
+            // Log state with SignalLogger class
+            state -> SignalLogger.writeString("Elevator/SysIdElevatorState", state.toString())
+        ) , 
+        new SysIdRoutine.Mechanism(
+            (voltage) -> setVoltage(voltage.in(Volts)),
+            null,
+            this
+        )
+    );
 
     public ElevatorSubsystem(ElevatorIO io){
         this.io = io;
