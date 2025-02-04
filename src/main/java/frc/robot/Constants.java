8d341e65a67d707dfe8d6276cccb052dfc58fe3a
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2025-02-04 09:31:24 -0500

    Update Constants.java to have consistent whitespace and capitalization

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,7 @@ import org.apache.commons.lang3.NotImplementedException;
  */
 public final class Constants {
 
-    public static class OIConstants{
+    public static class OIConstants {
         public static final int kDriverControllerPort = 0;
         public static final int kOperatorControllerPort = 1;
         public static final double kControllerDeadband = 0.15;
@@ -48,29 +48,29 @@ public final class Constants {
             public static final double singleClutchRotationFactor = 0.5;
             public static final double doubleClutchTranslationFactor = 0.3;
             public static final double doubleClutchRotationFactor = 0.35;
-            public static final double maxTranslationSpeedMPS = 5;
-            public static final double maxRotationSpeedRadPerSec = 5;
+            public static final double maxTranslationSpeedMPS = 5.0;
+            public static final double maxRotationSpeedRadPerSec = 5.0;
         }
 
         public static class PathPlannerConstants {
             public static final PathConstraints pathConstraints = new PathConstraints(4.69, 25, Units.degreesToRadians(1080), Units.degreesToRadians(1080));
             public static final Transform2d robotOffset = new Transform2d(0.4572, 0, Rotation2d.kZero);
-            public static final double pathGenerationToleranceMeters = 0.011; // technically it's anything larger than 0.01, but I'm adding .001 just to be safe
+            public static final double pathGenerationToleranceMeters = 0.011; // Technically it's anything larger than 0.01, but I'm adding .001 just to be safe
 
             public static class Control {
-                public static final PIDConstants transPID = new PIDConstants(5,0,0);
-                public static final PIDConstants rotPID = new PIDConstants(5,0,0);
+                public static final PIDConstants transPID = new PIDConstants(5, 0, 0);
+                public static final PIDConstants rotPID = new PIDConstants(5, 0, 0);
             }
         }
     }
 
     public static class SuperstructureConstants {
 
-        //Grabber Possession State
+        // Grabber Possession State
         public enum GrabberPossession {
-            EMPTY(0,0.6),
-            CORAL(0,0.6),
-            ALGAE(0,0.6);
+            EMPTY(0, 0.6),
+            CORAL(0, 0.6),
+            ALGAE(0, 0.6);
 
             public final double wrist_kG;
             public final double elevator_kG;
@@ -81,24 +81,23 @@ public final class Constants {
             }
         }
 
-        //Index Possession State
+        // Index Possession State
         public enum IndexPossession {
             EMPTY,
             CORAL
         }
 
-        //Grabber State
+        // Grabber State
         public enum GrabberState {
 
-            IDLE(0,0),
+            IDLE(0, 0),
             
-            ALGAE_INTAKE(-12,-12),
-            CORAL_INTAKE(5,5),
+            ALGAE_INTAKE(-12, -12),
+            CORAL_INTAKE(5, 5),
 
-            ALGAE_OUTTAKE(6,6),
-            CORAL_OUTTAKE(12,12),
-            DEFAULT_OUTTAKE(12,12);
-
+            ALGAE_OUTTAKE(6, 6),
+            CORAL_OUTTAKE(12, 12),
+            DEFAULT_OUTTAKE(12, 12);
 
             public final double leftVoltage;
             public final double rightVoltage;
@@ -109,38 +108,38 @@ public final class Constants {
             }
         }
 
-        //Index State
+        // Index State
         public enum IndexState {
             EMPTY_IDLE(false),
             CORAL_INTAKE(true),
             CORAL_TRANSFER(true),
             CORAL_IDLE(false);
-            public final boolean running; //Whether or not the indexer motors are running
+            public final boolean running; // Whether or not the indexer motors are running
             private IndexState(boolean running) {
                 this.running = running;
             }
         }
 
-        //Represent the elevator height and wrist angle for different positions, the full position of the grabber
-        //Should we have an eject state with an optional elevator height? just to immediately eject if a game piece is stuck
+        // Represent the elevator height and wrist angle for different positions, the full position of the grabber
+        // Should we have an eject state with an optional elevator height? just to immediately eject if a game piece is stuck
         public enum WristevatorState {
             
             TRAVEL(0.08128,90),
             ALGAE_TRAVEL(0.08128, 65),
 
-            CORAL_TRANSFER(0.08128,-23.5), //same as CORAL_DIRECT_INTAKE
+            CORAL_TRANSFER(0.08128,-23.5), // Same as CORAL_DIRECT_INTAKE
 
-            L1(-1,-1), //Placeholder
-            L2(0.71628,-35),
-            L3(1.11252,-35),
-            L4(1.8161,-45),
+            L1(-1, -1), // Placeholder
+            L2(0.71628, -35),
+            L3(1.11252, -35),
+            L4(1.8161, -45),
 
             GROUND_INTAKE(-1, -1),
-            LOW_INTAKE(0.9144,-35),
-            HIGH_INTAKE(1.30556,-35),
+            LOW_INTAKE(0.9144, -35),
+            HIGH_INTAKE(1.30556, -35),
 
-            PROCESSOR(0.184277,0),
-            NET(1.8288,65);
+            PROCESSOR(0.184277, 0),
+            NET(1.8288, 65);
 
             public final double elevatorHeightMeters;
             public final Rotation2d wristAngle;
@@ -164,7 +163,7 @@ public final class Constants {
         public enum ReefFace {
             // IMPORTANT: Fudge factors are always positive and should be in meters (use the Units.inchesToMeters() method)
 
-            //Blue Reef
+            // Blue Reef
             BLU_REEF_AB(18, 3.657600, 4.025900, 180.0, null, null),
             BLU_REEF_CD(17, 4.073906, 3.306318, 240.0, null, null),
             BLU_REEF_EF(22, 4.904740, 3.306318, 300.0, null, null),
@@ -172,7 +171,7 @@ public final class Constants {
             BLU_REEF_IJ(20, 4.904740, 4.745482, 60.0, null, null),
             BLU_REEF_KL(19, 4.073906, 4.745482, 120.0, null, null),
 
-            //Red Reef
+            // Red Reef
             RED_REEF_AB(7, 13.890498, 4.025900, 0.0, null, null),
             RED_REEF_CD(8, 13.474446, 4.745482, 60., null, null),
             RED_REEF_EF(9, 12.643358, 4.745482, 120.0, null, null),
@@ -209,10 +208,10 @@ public final class Constants {
             }
         }
 
-        //Generic rotation-agnostic points of interest
+        // Generic rotation-agnostic points of interest
         public enum PointOfInterest {
-            BLU_REEF(4.487,4.010),
-            RED_REEF(13.062,4.010);
+            BLU_REEF(4.487, 4.010),
+            RED_REEF(13.062, 4.010);
 
             public final Translation2d position;
             private PointOfInterest(double xMeters, double yMeters) {
@@ -220,7 +219,7 @@ public final class Constants {
             }
         }
 
-        //Poses of interest
+        // Poses of interest
         public enum PoseOfInterest {
             BLU_PROCESSOR(5.973318,-0.00381,90), //Taken from April Tag coordinates
             RED_PROCESSOR(11.56081,	8.05561,	270), //Taken from April Tag coordinates
@@ -242,22 +241,22 @@ public final class Constants {
         public static final int kMotorPort1 = -1;
         
         public static final double elevatorPositionToleranceMeters = Units.inchesToMeters(0.5);
-        public static final double maxOperatorControlVolts = 6;
+        public static final double maxOperatorControlVolts = 6.0;
 
         public static final boolean leadMotorInverted = false;
         public static final boolean followMotorInverted = false;
 
-        //Heights measured in meters
-        public static final double lowHeight = 0;
-        public static final double autonHeight = 0;
-        public static final double midHeight = 0;
-        public static final double highHeight = 0;
+        // Heights measured in meters
+        public static final double lowHeight = 0.0;
+        public static final double autonHeight = 0.0;
+        public static final double midHeight = 0.0;
+        public static final double highHeight = 0.0;
 
         //public static final double minHeightMeters = 0;
-        //public static final double maxHeightMeters = 0.85; //Temporary
+        //public static final double maxHeightMeters = 0.85; // Temporary
 
-        //https://wcproducts.com/collections/gearboxes/products/wcp-single-stage-gearbox  Inches.of(0.25).in(Meters)
-        // still set to WAPUR elevator units, need to be changed
+        // https://wcproducts.com/collections/gearboxes/products/wcp-single-stage-gearbox  Inches.of(0.25).in(Meters)
+        // Still set to WAPUR elevator units, need to be changed
         public static final double kVelocityConversionFactor = (11/60.0) * 22 * 0.00635 / 60.0; //Gear ratio & chain pitch & rpm -> m/s
         public static final double kPositionConversionFactor = (11/60.0) * 22 * 0.00635; //Gear ratio & chain pitch
         public static class Electrical {
@@ -267,44 +266,44 @@ public final class Constants {
 
 
         public static class Control {
-            //PID Constants - STILL SET TO WAPUR ELEVATOR VALUES
-            public static final double kP = 8;
+            // PID constants - STILL SET TO WAPUR ELEVATOR VALUES
+            public static final double kP = 8.0;
             public static final double kI = 0.0;
             public static final double kD = 0.0;
 
-            //Feedforward Constants - STILL SET TO WAPUR ELEVATOR VALUES
-            public static final double kS = 0; //Static gain (voltage)
+            // Feedforward constants - STILL SET TO WAPUR ELEVATOR VALUES
+            public static final double kS = 0.0; //Static gain (voltage)
             public static final double kG = 0.6; //Gravity gain (voltage)
-            public static final double kV = 12; // velocity game
-            public static final double kA = 0; //Acceleration Gain
+            public static final double kV = 12.0; // velocity game
+            public static final double kA = 0.0; //Acceleration Gain
         }
     }
 
     public static class ElevatorSimConstants {
-        //RANDOM ports
+        // RANDOM ports
         public static final int kSimMotorPort0 = 20;
         public static final int kSimMotorPort1 = 21;
         
         public static final double kElevatorGearing = 60.0/11.0;
         public static final double kCarriageMass = Units.lbsToKilograms(30); //kg
         public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0); //taken from example, do not know what this is
-        public static final double kMinElevatorHeightMeters = 0;
+        public static final double kMinElevatorHeightMeters = 0.0;
         public static final double kMaxElevatorHeightMeters = Units.inchesToMeters(72.0);
 
         public static final int kEncoderAChannel = 0;
         public static final int kEncoderBChannel = 1;
 
         public static class Control {
-            //PID Constants - STILL SET TO WAPUR ELEVATOR VALUES
-            public static final double kP = 6;
+            // PID constants - STILL SET TO WAPUR ELEVATOR VALUES
+            public static final double kP = 6.0;
             public static final double kI = 0.0;
             public static final double kD = 0.0;
 
-            //Feedforward Constants - STILL SET TO WAPUR ELEVATOR VALUES
-            public static final double kS = 0; // Static gain (voltage)
+            // Feedforward constants - STILL SET TO WAPUR ELEVATOR VALUES
+            public static final double kS = 0.0; // Static gain (voltage)
             public static final double kG = 2.8605; // Gravity gain (voltage)
-            public static final double kV = 0; // velocity gain
-            public static final double kA = 0; // Acceleration Gain
+            public static final double kV = 0.0; // Velocity gain
+            public static final double kA = 0.0; // Acceleration Gain
         }
     }
 
@@ -312,7 +311,7 @@ public final class Constants {
         public static final int kLeftMotorPort = -1;
         public static final int kRightMotorPort = -1;
         
-        public static final double kCurrentLimit = 40; 
+        public static final double kCurrentLimit = 40.0; 
     }
 
     public static class WristConstants {
@@ -320,12 +319,12 @@ public final class Constants {
         public static final int kFollowMotorPort = -3;
 
         public static final double wristAngleToleranceRadians = Units.degreesToRadians(1);
-        public static final double maxOperatorControlVolts = 6;
+        public static final double maxOperatorControlVolts = 6.0;
 
         public static final boolean kLeadMotorInverted = false;
         public static final boolean kFollowMotorInverted = false;
 
-        // source: https://docs.revrobotics.com/brushless/spark-max/encoders/alternate-encoder
+        // Source: https://docs.revrobotics.com/brushless/spark-max/encoders/alternate-encoder
         public static final int kCountsPerRevolution = 8192;
         public static final double kPositionConversionFactor = 2*Math.PI; // rotations to radians
         public static final double kVelocityConversionFactor = (2*Math.PI) / 60.0; // rpm to radians/second
@@ -336,7 +335,7 @@ public final class Constants {
             public static final double kI = 0.0;
             public static final double kD = 0.0;
 
-            // feed forward constants
+            // Feedforward constants
             public static final double kS = 0.0; // static gain in volts
             public static final double kG = 0.0; // gravity gain in volts
             public static final double kV = 0.0; // velocity gain in volts per radian per second
@@ -345,17 +344,17 @@ public final class Constants {
     }
 
     public static class WristSimConstants {
-        // values are NOT CORRECT
-        public static final double kWristGearingReductions = 125;
-        public static final double kWristLength = Units.feetToMeters(1); // excludes a 5 inch fixed piece
-        public static final double kWristMass = 2;
+        // Values are NOT CORRECT
+        public static final double kWristGearingReductions = 125.0;
+        public static final double kWristLength = Units.feetToMeters(1); // Excludes a 5 inch fixed piece
+        public static final double kWristMass = 2.0;
         public static final double kMinAngleRads = -0.75 * Math.PI;
         public static final double kMaxAngleRads = 0.75 * Math.PI;
 
         public static class Control {
-            public static final double kP = 18;
-            public static final double kI = 0;
-            public static final double kD = 0;
+            public static final double kP = 18.0;
+            public static final double kI = 0.0;
+            public static final double kD = 0.0;
 
             public static final double kS = 0.0; // static gain in volts
             public static final double kG = 0.0553; // gravity gain in volts
@@ -368,8 +367,8 @@ public final class Constants {
         public static final int kLeadMotorPort = -10;
         public static final int kFollowMotorPort = -10;
 
-        public static final double kCurrentLimit = 40;
-        public static final double kIndexVoltage = 6;
+        public static final double kCurrentLimit = 40.0;
+        public static final double kIndexVoltage = 6.0;
 
         public static final boolean kLeadMotorInverted = false;
         public static final boolean kFollowMotorInverted = false;
