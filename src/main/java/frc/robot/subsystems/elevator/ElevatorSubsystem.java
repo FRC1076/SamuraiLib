72940b1372db61779e20c68840f8a665b472686f
Jesse Kane <jessekane66@gmail.com>
2025-02-16 22:40:28 -0500

    first commit
    Co-authored-by: Dylan Li <Cryplo@users.noreply.github.com>
    Co-authored-by: David Loh <ldavidc@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -2,11 +2,13 @@ package frc.robot.subsystems.elevator;
 
 import frc.robot.Constants;
 import frc.robot.Constants.ElevatorConstants;
-
+import lib.control.MutableElevatorFeedforward;
 import lib.utils.MathHelpers;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
@@ -18,9 +20,28 @@ import org.littletonrobotics.junction.Logger;
 
 import com.ctre.phoenix6.SignalLogger;
 
+import static frc.robot.Constants.ElevatorConstants.Control.kA;
+import static frc.robot.Constants.ElevatorConstants.Control.kD;
+import static frc.robot.Constants.ElevatorConstants.Control.kI;
+import static frc.robot.Constants.ElevatorConstants.Control.kP;
+import static frc.robot.Constants.ElevatorConstants.Control.kS;
+import static frc.robot.Constants.ElevatorConstants.Control.kV;
+import static frc.robot.Constants.ElevatorConstants.Control.kG;
+
 public class ElevatorSubsystem extends SubsystemBase {
+    private static enum ControlMode {
+        kVoltage,
+        kVelocity,
+        kPosition
+    }
+    private ControlMode mode = ControlMode.kVoltage;
     private final ElevatorIO io;
     private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+    private final ProfiledPIDController m_profiledPIDController = 
+        new ProfiledPIDController(kP, kI, kD, new Constraints(1, 3));
+
+    private final MutableElevatorFeedforward m_feedforwardController = new MutableElevatorFeedforward(kS, kG, kV, kA);
+
     private final SysIdRoutine m_elevatorSysIdRoutine = new SysIdRoutine(
         new SysIdRoutine.Config(
             null,        // Use default ramp rate (1 V/s)
@@ -45,6 +66,16 @@ public class ElevatorSubsystem extends SubsystemBase {
         //System.out.println("Elevator: " + this.getPositionMeters());
         io.updateInputs(inputs);
         Logger.processInputs("Elevator",inputs);
+        switch (mode) {
+            case kVoltage:
+                break;
+            case kVelocity:
+                break; //TODO: Velocity control has not yet been implemented
+            case kPosition:
+                io.setVoltage(
+                    m_profiledPIDController.calculate(inputs.elevatorHeightMeters) + m_feedforwardController.getKg()
+                );
+        }
     }
 
     @Override
@@ -66,15 +97,12 @@ public class ElevatorSubsystem extends SubsystemBase {
      */
     public void setVoltage(double volts) {
         
-        
-        if(this.getPositionMeters() > ElevatorConstants.kMaxElevatorHeightMeters && volts > 0) {
-            volts = 0; //TODO: make this kG instead of 0?
+        if (this.getPositionMeters() > ElevatorConstants.kMaxElevatorHeightMeters && volts > 0) {
+            volts = m_feedforwardController.getKg();
+        } else if (this.getPositionMeters() < ElevatorConstants.kMinElevatorHeightMeters && volts < 0) {
+            volts = m_feedforwardController.getKg();
         }
-        else if(this.getPositionMeters() < ElevatorConstants.kMinElevatorHeightMeters && volts < 0) {
-            volts = 0;
-        }
-        
-
+    
         io.setVoltage(volts);
     }
 
@@ -82,7 +110,7 @@ public class ElevatorSubsystem extends SubsystemBase {
      * @param kg New kG value in volts
      */
     public void setKg(double kg) {
-        this.io.setFFkG(kg);
+        m_feedforwardController.setKg(kg);
     }
 
     /** Returns position of the elevator, as a double */
